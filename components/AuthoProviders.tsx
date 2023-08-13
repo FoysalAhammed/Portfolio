f19a57d66cf056b6329933f8f7a58bd532449ed6
FoysalAhammed <hanif.mia2233@gmail.com>
2023-08-14 00:20:40 +0600

    first commit

---
+++ components/AuthoProviders.tsx
@@ -0,0 +1,5 @@
+const AuthoProviders = () => {
+  return <div>AuthoProviders</div>;
+};
+
+export default AuthoProviders;
---
+++ grafbase/grafbase.config.ts
@@ -0,0 +1,24 @@
+import { config, g } from "@grafbase/sdk";
+const User = g.model("User", {
+  name: g.string().length({ min: 2, max: 20 }),
+  email: g.string.unique(),
+  avatarUrl: g.url(),
+  description: g.string().optional(),
+  githubUrl: g.url().optional(),
+  linedInUrl: g.url().optional(),
+  projects: g
+    .relation(() => Project)
+    .list()
+    .optional(),
+});
+const Project = g.model("project", {
+  title: g.string().length({ min: 3 }),
+  description: g.string(),
+  image: g.url(),
+  liveSiteUrl: g.string(),
+  category: g.string().search(),
+  createdBy: g.relation(() => User),
+});
+export default config({
+  schema: g,
+});
