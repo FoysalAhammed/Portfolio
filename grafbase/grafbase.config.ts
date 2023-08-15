5f8cacadb91435e4eb819df869219424232554b1
FoysalAhammed <hanif.mia2233@gmail.com>
2023-08-15 13:45:39 +0600

    first commit

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,7 +1,7 @@
 import { config, g } from "@grafbase/sdk";
 const User = g.model("User", {
   name: g.string().length({ min: 2, max: 20 }),
-  email: g.string.unique(),
+  email: g.string().unique(),
   avatarUrl: g.url(),
   description: g.string().optional(),
   githubUrl: g.url().optional(),
