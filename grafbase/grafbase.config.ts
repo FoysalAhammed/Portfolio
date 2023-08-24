f2f3b24d65354838308a8acea6ca7b6074ca3607
FoysalAhammed <hanif.mia2233@gmail.com>
2023-08-24 10:59:09 +0600

    first commit

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,24 +1,30 @@
 import { config, g } from "@grafbase/sdk";
+
+// @ts-ignore
 const User = g.model("User", {
-  name: g.string().length({ min: 2, max: 20 }),
+  name: g.string().length({ min: 2, max: 100 }),
   email: g.string().unique(),
   avatarUrl: g.url(),
-  description: g.string().optional(),
+  description: g.string().length({ min: 2, max: 1000 }).optional(),
   githubUrl: g.url().optional(),
-  linedInUrl: g.url().optional(),
+  linkedinUrl: g.url().optional(),
   projects: g
     .relation(() => Project)
     .list()
     .optional(),
 });
-const Project = g.model("project", {
+
+// @ts-ignore
+const Project = g.model("Project", {
   title: g.string().length({ min: 3 }),
   description: g.string(),
   image: g.url(),
-  liveSiteUrl: g.string(),
+  liveSiteUrl: g.url(),
+  githubUrl: g.url(),
   category: g.string().search(),
   createdBy: g.relation(() => User),
 });
+
 export default config({
   schema: g,
 });
